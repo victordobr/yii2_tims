1899c31285c78fcba617dc4aac2880b6741475f8
Victor Dobrianskiy <victordobr@gmail.com>
2016-01-05 15:59:55 +0200

    Add debug method "Pa"

--- base/Module.php
+++ base/Module.php
@@ -50,4 +50,18 @@ class Module extends \yii\base\Module
             ],
         ];
     }
+
+    /**
+     * Helper for debug
+     * @param mixed $mixed
+     * @param boolean $stop
+     */
+    public static function pa($mixed, $stop = false){
+        $ar = debug_backtrace();
+        $key = pathinfo($ar[0]['file']);
+        $key = $key['basename'] .':'. $ar[0]['line'];
+        $print = array($key => $mixed);
+        echo '<pre>'. print_r($print, 1) .'</pre>';
+        if ($stop == 1) exit();
+    }
 }
