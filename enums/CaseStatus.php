b578ebaee1511fee8c7edc6ac1c1ad488fd72ca3
eugene <egichko@entenso.com>
2016-01-18 13:25:00 +0200

    added enum police case statuses

---
+++ enums/CaseStatus.php
@@ -0,0 +1,16 @@
+<?php
+namespace app\enums;
+
+use \Yii;
+use \kfosoft\base\Enum;
+
+/**
+ * UserType Enum
+ * @package app\enums
+ */
+class CaseStatus extends Enum
+{
+    const INCOMPLETE = 1010;
+    const COMPLETE = 1020;
+    const FULL_COMPLETE = 1021;
+}
