72813eddc023723d33aface9407afd6eecb58aa6
Alex Makhorin <despected@gmail.com>
2016-01-21 13:28:51 +0200

    migration fix

--- migrations/m160120_123818_File_change_foreign_key.php
+++ migrations/m160120_123818_File_change_foreign_key.php
@@ -5,6 +5,12 @@ use yii\db\Migration;
 class m160120_123818_File_change_foreign_key extends Migration
 {
     const TABLE = 'File';
+
+    private $columns = [
+        'evidence_id' => 'record_id',
+        'evidence_file_type' => 'record_file_type',
+    ];
+
     public function safeUp()
     {
         $this->execute('SET FOREIGN_KEY_CHECKS = 0;');
@@ -12,12 +18,21 @@ class m160120_123818_File_change_foreign_key extends Migration
         $this->dropForeignKey('FK_File_Evidence', self::TABLE);
         $this->addForeignKey('FK_File_Record', self::TABLE, 'record_id', 'Record', 'id');
         $this->execute('SET FOREIGN_KEY_CHECKS = 1;');
+
+        foreach ($this->columns as $name => $newName) {
+            $this->renameColumn(self::TABLE, $name, $newName);
+        }
     }
+
     public function safeDown()
     {
         $this->execute('SET FOREIGN_KEY_CHECKS = 0;');
         $this->dropIndex('FK_File_Record', self::TABLE);
         $this->dropForeignKey('FK_File_Record', self::TABLE);
         $this->execute('SET FOREIGN_KEY_CHECKS = 1;');
+
+        foreach ($this->columns as $name => $newName) {
+            $this->renameColumn(self::TABLE, $newName, $name);
+        }
     }
 }
