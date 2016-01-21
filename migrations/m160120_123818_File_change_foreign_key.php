c5913ab0f55a76b593960482fec2bdfdbc7460d2
Alex Makhorin <despected@gmail.com>
2016-01-21 13:31:38 +0200

    migration fix2

--- migrations/m160120_123818_File_change_foreign_key.php
+++ migrations/m160120_123818_File_change_foreign_key.php
@@ -16,12 +16,14 @@ class m160120_123818_File_change_foreign_key extends Migration
         $this->execute('SET FOREIGN_KEY_CHECKS = 0;');
         $this->dropIndex('FK_File_Evidence', self::TABLE);
         $this->dropForeignKey('FK_File_Evidence', self::TABLE);
-        $this->addForeignKey('FK_File_Record', self::TABLE, 'record_id', 'Record', 'id');
-        $this->execute('SET FOREIGN_KEY_CHECKS = 1;');
+
 
         foreach ($this->columns as $name => $newName) {
             $this->renameColumn(self::TABLE, $name, $newName);
         }
+
+        $this->addForeignKey('FK_File_Record', self::TABLE, 'record_id', 'Record', 'id');
+        $this->execute('SET FOREIGN_KEY_CHECKS = 1;');
     }
 
     public function safeDown()
