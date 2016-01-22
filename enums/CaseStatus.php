e772239e9c24f1f8fc6be6fa745907835ff8f673
eugene <egichko@entenso.com>
2016-01-22 19:04:54 +0200

    added helper methods

--- enums/CaseStatus.php
+++ enums/CaseStatus.php
@@ -2,6 +2,7 @@
 namespace app\enums;
 
 use \Yii;
+use app\models\User;
 use \kfosoft\base\Enum;
 
 /**
@@ -13,4 +14,17 @@ class CaseStatus extends Enum
     const INCOMPLETE = 1010;
     const COMPLETE = 1020;
     const FULL_COMPLETE = 1021;
+
+    public static function getByUserRole()
+    {
+        switch (true) {
+            case User::hasRole(Role::ROLE_VIDEO_ANALYST):
+                return [self::INCOMPLETE, self::COMPLETE, self::FULL_COMPLETE];
+            case User::hasRole(Role::ROLE_VIDEO_ANALYST_SUPERVISOR):
+                return [self::COMPLETE, self::FULL_COMPLETE];
+            default:
+                return [];
+        }
+    }
+
 }
