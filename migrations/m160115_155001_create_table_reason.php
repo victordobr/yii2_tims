259353a667cca2d038655cf1a2101c90f01aac4b
Alex Makhorin <despected@gmail.com>
2016-01-24 17:44:07 +0200

    migr fixes

--- migrations/m160115_155001_create_table_reason.php
+++ migrations/m160115_155001_create_table_reason.php
@@ -6,15 +6,21 @@ use yii\db\Migration;
 class m160115_155001_create_table_reason extends Migration
 {
     const TABLE = 'Reason';
-    private $columns = [
-        'id' => Schema::TYPE_PK,
-        'code' => Schema::TYPE_INTEGER . ' NOT NULL DEFAULT 0',
-        'description' => Schema::TYPE_TEXT . ' NOT NULL DEFAULT ""',
-    ];
 
     public function up()
     {
-        $this->createTable(self::TABLE, $this->columns, 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB');
+        $this->execute("
+            CREATE TABLE IF NOT EXISTS `Reason` (
+              `id` int(11) NOT NULL AUTO_INCREMENT,
+              `code` int(11) NOT NULL DEFAULT '0',
+              `description` text COLLATE utf8_unicode_ci NOT NULL,
+              PRIMARY KEY (`id`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+            DELETE FROM `Reason`;
+            INSERT INTO `Reason` (`id`, `code`, `description`) VALUES
+                (1, 10, 'fgh');
+        ");
     }
 
     public function down()
