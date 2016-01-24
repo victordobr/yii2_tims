504eb90028b42206238554b4881dd524b5bba428
Alex Makhorin <despected@gmail.com>
2016-01-24 17:59:50 +0200

    migr fixes

---
+++ migrations/m160124_155622_fix_open_date.php
@@ -0,0 +1,19 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m160124_155622_fix_open_date extends Migration
+{
+    public function safeUp()
+    {
+        $this->execute("
+          UPDATE `Record` SET `open_date`=NULL;
+        ");
+    }
+
+    public function safeDown()
+    {
+
+    }
+}
