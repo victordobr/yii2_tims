cc587073a493b404159e983c34721b342501d28e
eugene <egichko@entenso.com>
2016-01-27 19:36:36 +0200

    fix style on deactivate form

--- modules/frontend/views/forms/request-deactivation.php
+++ modules/frontend/views/forms/request-deactivation.php
@@ -3,11 +3,12 @@
  * @var $action string
  * @var $model \app\modules\frontend\models\form\RequestDeactivateForm
  */
+
 use yii\helpers\Html;
 use kartik\form\ActiveForm;
-
 ?>
-<div class="col-xs-12">
+
+<div class="col-lg-12">
     <div class="panel panel-default">
         <div class="panel-heading"><?= Yii::t('app', 'Request deactivation'); ?></div>
         <div class="panel-body">
@@ -26,6 +27,7 @@ use kartik\form\ActiveForm;
             ]); ?>
 
             <fieldset>
+
                 <div class="form-group badge-step">
                     <div class="col-lg-12">
                         <span class="badge"><?= Yii::t('app', 'STEP 1') ?></span>
@@ -38,9 +40,11 @@ use kartik\form\ActiveForm;
                     'maxlength' => true,
                     'class' => 'form-control form-field-short',
                 ]) ?>
+
             </fieldset>
 
             <fieldset>
+
                 <div class="form-group badge-step">
                     <div class="col-lg-12">
                         <span class="badge"><?= Yii::t('app', 'STEP 2') ?></span>
@@ -52,6 +56,7 @@ use kartik\form\ActiveForm;
                         <?= Html::submitButton('Submit', ['class' => 'btn btn-primary col-lg-1']) ?>
                     </div>
                 </div>
+
             </fieldset>
 
             <?php ActiveForm::end(); ?>
