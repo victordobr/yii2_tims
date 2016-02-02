c3505f7f0db9846d06ea42f24fee6f7cb6027329
eugene <egichko@entenso.com>
2016-02-02 16:52:18 +0200

    modify StatusHistory table

--- migrations/m160126_112512_create_table_StatusHistory.php
+++ migrations/m160126_112512_create_table_StatusHistory.php
@@ -9,6 +9,7 @@ class m160126_112512_create_table_StatusHistory extends Migration
 
     public function up()
     {
+        $this->execute('SET FOREIGN_KEY_CHECKS=0');
         $this->createTable(
             self::TABLE,
             [
@@ -27,6 +28,7 @@ class m160126_112512_create_table_StatusHistory extends Migration
         $this->addForeignKey('FK_Status_Record', self::TABLE, ['record_id'], 'Record', ['id'], 'CASCADE', 'CASCADE');
         $this->addForeignKey('FK_Status_User', self::TABLE, ['author_id'], 'User', ['id'], 'CASCADE', 'CASCADE');
         $this->addForeignKey('FK_Status_Reason', self::TABLE, ['reason_code'], 'Reason', ['code'], 'CASCADE', 'CASCADE');
+        $this->execute('SET FOREIGN_KEY_CHECKS=1');
     }
 
     public function down()
