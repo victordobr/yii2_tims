390c72df867562e36fa8441b75e4da8ed62dfa80
eugene <egichko@entenso.com>
2016-02-03 12:48:47 +0200

    moved render and styles to widget for ActiveForm

--- modules/frontend/views/forms/deactivate.php
+++ modules/frontend/views/forms/deactivate.php
@@ -5,85 +5,78 @@
  */
 
 use yii\helpers\Html;
-use kartik\form\ActiveForm;
+use app\widgets\base\ActiveForm;
 
 ?>
 
 <div class="col-lg-12">
-    <div class="panel panel-default">
-        <div class="panel-heading"><?= Yii::t('app', 'Request deactivation'); ?></div>
-        <div class="panel-body">
 
-            <?php $form = ActiveForm::begin([
-                'id' => 'deactivate-form',
-                'action' => $action,
-                'enableClientValidation' => true,
-                'options' => [
-                    'class' => 'form-horizontal',
-                    'data-pjax' => true
-                ],
-                'fieldConfig' => [
-                    'template' => "<div class=\"col-lg-12\">{label}\n{input}</div>\n<div class=\"col-lg-11 col-lg-offset-1\">{error}</div>",
-                ],
-            ]); ?>
+    <?php $form = ActiveForm::begin([
+        'title' => Yii::t('app', 'Request deactivation'),
+        'id' => 'deactivate-form',
+        'action' => $action,
+        'enableClientValidation' => true,
+        'options' => [
+            'class' => 'form-horizontal',
+            'data-pjax' => true
+        ],
+        'fieldConfig' => [
+            'template' => "<div class=\"col-lg-12\">{label}\n{input}</div>\n<div class=\"col-lg-11 col-lg-offset-1\">{error}</div>",
+        ],
+    ]); ?>
 
-            <div class="col-lg-6">
+    <div class="col-lg-6">
 
-                <div class="form-group badge-step">
-                    <div class="col-lg-12">
-                        <span class="badge"><?= Yii::t('app', 'STEP 1') ?></span>
-                    </div>
-                </div>
+        <legend class="badge-step">
+            <span class="badge"><?= Yii::t('app', 'STEP 1') ?></span>
+        </legend>
 
-                <?= $form->field($model, 'requested_by')->textInput([
-                    'maxlength' => true,
-                    'class' => 'form-control form-field-short',
-                    'disabled' => 'disabled'
-                ]); ?>
+        <?= $form->field($model, 'requested_by')->textInput([
+            'maxlength' => true,
+            'class' => 'form-control form-field-short',
+            'disabled' => 'disabled'
+        ]); ?>
 
-                <?= $form->field($model, 'review_reason')->textInput([
-                    'maxlength' => true,
-                    'class' => 'form-control form-field-short',
-                    'disabled' => 'disabled'
-                ]); ?>
+        <?= $form->field($model, 'review_reason')->textInput([
+            'maxlength' => true,
+            'class' => 'form-control form-field-short',
+            'disabled' => 'disabled'
+        ]); ?>
 
-                <div class="form-group badge-step">
-                    <div class="col-lg-12">
-                        <span class="badge"><?= Yii::t('app', 'STEP 2') ?></span>
-                    </div>
-                </div>
+        <legend class="badge-step">
+            <span class="badge"><?= Yii::t('app', 'STEP 2') ?></span>
+        </legend>
 
-                <?= $form->field($model, 'action')->radioList($model->actions(), [
-                    'onclick' => 'js:$("#approve-deactivation-step-3").toggle($(this).find("input[type=radio]:checked").val() == "reject");'
-                ])->label(false); ?>
+        <?= $form->field($model, 'action')->radioList($model->actions(), [
+            'onclick' => 'js:$("#approve-deactivation-step-3").toggle($(this).find("input[type=radio]:checked").val() == "reject");'
+        ])->label(false); ?>
 
-            </div>
+    </div>
 
-            <div id="approve-deactivation-step-3" class="col-lg-6" style="display: none">
+    <div class="col-lg-6">
 
-                <div class="form-group badge-step">
-                    <div class="col-lg-12">
-                        <span class="badge"><?= Yii::t('app', 'STEP 3') ?></span>
-                    </div>
-                </div>
+        <div id="approve-deactivation-step-3" style="display: none">
 
-                <?= $form->field($model, 'code')->dropDownList([10 => 10]); ?>
+            <legend class="badge-step">
+                <span class="badge"><?= Yii::t('app', 'STEP 3') ?></span>
+            </legend>
 
-                <?= $form->field($model, 'description')->textInput([
-                    'maxlength' => true,
-                    'class' => 'form-control form-field-short',
-                ]) ?>
+            <?= $form->field($model, 'code')->dropDownList([10 => 10]); ?>
 
-            </div>
+            <?= $form->field($model, 'description')->textInput([
+                'maxlength' => true,
+                'class' => 'form-control form-field-short',
+            ]) ?>
+        </div>
 
-            <div class="form-group">
-                <div class="col-lg-12">
-                    <?= Html::submitButton('Submit', ['class' => 'btn btn-primary col-lg-1 pull-right']) ?>
-                </div>
-            </div>
+    </div>
 
-            <?php ActiveForm::end(); ?>
-
+    <div class="form-group">
+        <div class="col-lg-12">
+            <?= Html::submitButton('Submit', ['class' => 'btn btn-primary pull-right']) ?>
         </div>
     </div>
+
+    <?php ActiveForm::end(); ?>
+
 </div>
