5cb75da41d0a6735cbf78ec38cb94a4527414f5e
eugene <egichko@entenso.com>
2016-02-04 15:26:55 +0200

    added number validator to input X days in filter

--- widgets/record/filter/js/index.js
+++ widgets/record/filter/js/index.js
@@ -16,7 +16,7 @@ $(function () {
     });
 
     form.on('keyup', 'input[name="Record[X]"]', function (e) {
-        this.value = this.value.replace(/[^0-9\.]/g, '');
+        this.value = this.value.replace(/[^0-9]/g, '');
     });
 
 });
