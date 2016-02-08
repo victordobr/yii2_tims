d16a0b598948d1d1a58886046d28d869326c04c7
eugene <egichko@entenso.com>
2016-02-08 11:13:23 +0200

    added print preview page

--- modules/frontend/views/layouts/preview.php
+++ modules/frontend/views/layouts/preview.php
@@ -3,9 +3,8 @@
  * @var \yii\web\View $this
  * @var string $content
  */
-app\assets\PrintAsset::register($this);
-
 ?>
+
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
 <html lang="<?= \Yii::$app->language ?>">
