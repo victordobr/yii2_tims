4458bbf49969ea35b0f61a1e61215c6facf81eb5
eugene <egichko@entenso.com>
2016-02-09 17:17:58 +0200

    refactor search model of Record

--- modules/frontend/models/search/Record.php
+++ modules/frontend/models/search/Record.php
@@ -4,45 +4,33 @@ namespace app\modules\frontend\models\search;
 use app\enums\CaseStatus as Status;
 use app\models\StatusHistory;
 use app\models\User;
+use app\modules\frontend\models\base\RecordFilter;
 use Yii;
-use yii\data\ActiveDataProvider;
 use yii\db\ActiveQuery;
-use yii\db\Expression;
 use yii\db\QueryInterface;
 use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
 
-class Record extends \app\modules\frontend\models\base\Record
+class Record extends \app\modules\frontend\models\base\Record implements RecordFilter
 {
-    const CREATED_AT_TODAY = 1;
-    const CREATED_AT_LAST_3_DAYS = 2;
-    const CREATED_AT_LAST_X_DAYS = 3;
-    const CREATED_ALL = 4;
+    const FILTER_CREATED_AT_TODAY = 1;
+    const FILTER_CREATED_AT_LAST_3_DAYS = 2;
+    const FILTER_CREATED_AT_LAST_X_DAYS = 3;
+    const FILTER_CREATED_ALL = 4;
 
-    const STATUS_INCOMPLETE = 1;
-    const STATUS_COMPLETE_WITH_DEACTIVATION_WINDOW = 2;
-    const STATUS_PRINT_P1 = 3;
-    const STATUS_RE_PRINT = 4;
+    const FILTER_STATUS_DISABLED = 0;
+    const FILTER_STATUS_INCOMPLETE = 1;
+    const FILTER_STATUS_COMPLETE = 2;
 
-    public $filter_created_at_list = [];
-    public $uploader_list = [];
-
-    public $filter_created_at = self::CREATED_ALL;
-    public $filter_status = 0;
-    public $filter_status_incomplete = 0;
-    public $filter_status_complete = 0;
-    public $filter_uploaded_by = 0;
-
-    public $by_infraction_date;
-    public $by_status;
-    public $uploaded_by;
-    public $author_id;
+    public $filter_created_at = self::FILTER_CREATED_ALL;
+    public $filter_status = self::FILTER_STATUS_DISABLED;
+    public $filter_author_id;
 
     public function attributeLabels()
     {
         return ArrayHelper::merge(parent::attributeLabels(), [
             'id' => Yii::t('app', '#'),
-            'author_id' => Yii::t('app', 'Uploader By'),
+            'filter_author_id' => Yii::t('app', 'Uploader By'),
             'created_at' => Yii::t('app', 'Uploaded Date'),
             'license' => Yii::t('app', 'Vehicle Tag #'),
             'elapsedTime' => Yii::t('app', 'Elapsed Time'),
@@ -56,8 +44,8 @@ class Record extends \app\modules\frontend\models\base\Record
 
         $this->setAttributes($params);
 
-        if (!empty($params['author_id'])) {
-            $this->filterByAuthorID($query, $params['author_id']);
+        if (!empty($params['filter_author_id'])) {
+            $this->filterByAuthorID($query, $params['filter_author_id']);
         }
 
         if (!empty($params['filter_created_at'])) {
@@ -71,41 +59,6 @@ class Record extends \app\modules\frontend\models\base\Record
         return $provider;
     }
 
-    public function preview($params)
-    {
-        $query = $this->find()
-            ->select([
-                'id' => 'record.id',
-                'license' => 'record.license',
-                'lat' => 'record.lat',
-                'lng' => 'record.lng',
-                'state_id' => 'record.state_id',
-                'infraction_date' => 'record.infraction_date',
-                'created_at' => 'record.created_at',
-                'status_id' => 'record.status_id',
-                'elapsedTime' => self::SQL_SELECT_ELAPSED_TIME,
-            ])
-            ->from(['record' => static::tableName()])
-            ->joinWith([
-                'user' => function ($query) {
-                    $query->from('User user');
-                },
-                'owner' => function ($query) {
-                    $query->from('Owner owner');
-                },
-            ]);
-
-        $provider = new ActiveDataProvider(['query' => $query]);
-
-        $query->andFilterWhere(['like', self::SQL_SELECT_ELAPSED_TIME, $this->elapsedTime]);
-
-        if (!empty($params['ids'])) {
-            $query->andFilterWhere(['in', 'record.id', $params['ids']]);
-        }
-
-        return $provider;
-    }
-
     private function filterByAuthorID(QueryInterface &$query, $author_id)
     {
         $query->andWhere(['StatusHistory.author_id' => $author_id]);
@@ -114,19 +67,12 @@ class Record extends \app\modules\frontend\models\base\Record
     private function filterByStatus(QueryInterface &$query, $filter)
     {
         $statuses = [];
-        if (in_array(self::STATUS_INCOMPLETE, $filter)) {
+        if (in_array(self::FILTER_STATUS_INCOMPLETE, $filter)) {
             $statuses[] = Status::INCOMPLETE;
         }
-        if (in_array(self::STATUS_COMPLETE_WITH_DEACTIVATION_WINDOW, $filter)) {
+        if (in_array(self::FILTER_STATUS_COMPLETE, $filter)) {
             $statuses[] = Status::COMPLETE;
             $statuses[] = Status::FULL_COMPLETE;
-        }
-        if (in_array(self::STATUS_PRINT_P1, $filter)) {
-            $statuses[] = Status::DMV_DATA_RETRIEVED_COMPLETE;
-            $statuses[] = Status::DMV_DATA_RETRIEVED_INCOMPLETE;
-        }
-        if (in_array(self::STATUS_RE_PRINT, $filter)) {
-            $statuses[] = Status::QC_BAD_P1;
         }
         if (!empty($statuses)) {
             $query->andFilterWhere(['in', 'status_id', $statuses]);
@@ -140,11 +86,11 @@ class Record extends \app\modules\frontend\models\base\Record
         }
 
         switch ($filter) {
-            case self::CREATED_AT_TODAY:
+            case self::FILTER_CREATED_AT_TODAY:
                 return $query->andFilterWhere(['>', 'record.created_at', strtotime('midnight')]);
-            case self::CREATED_AT_LAST_3_DAYS:
+            case self::FILTER_CREATED_AT_LAST_3_DAYS:
                 return $query->andFilterWhere(['>', 'record.created_at', strtotime('-3 days')]);
-            case self::CREATED_AT_LAST_X_DAYS:
+            case self::FILTER_CREATED_AT_LAST_X_DAYS:
                 if (!is_numeric($days_ago) || $days_ago <= 0 || $days_ago > 366) {
                     return false;
                 }
@@ -152,37 +98,45 @@ class Record extends \app\modules\frontend\models\base\Record
         }
     }
 
-    private function initCreatedAtFilters()
+    public function getCreatedAtFilters()
     {
         $input = Html::input('text', 'Record[X]', '', ['maxlength' => 3]);
 
-        return $this->filter_created_at_list = [
-            self::CREATED_AT_TODAY => Yii::t('app', 'Today'),
-            self::CREATED_AT_LAST_3_DAYS => Yii::t('app', 'Last 3 days'),
-            self::CREATED_AT_LAST_X_DAYS => Yii::t('app', 'Last ') . $input . Yii::t('app', ' days'),
-            self::CREATED_ALL => Yii::t('app', 'All cases '),
+        return [
+            self::FILTER_CREATED_AT_TODAY => Yii::t('app', 'Today'),
+            self::FILTER_CREATED_AT_LAST_3_DAYS => Yii::t('app', 'Last 3 days'),
+            self::FILTER_CREATED_AT_LAST_X_DAYS => Yii::t('app', 'Last ') . $input . Yii::t('app', ' days'),
+            self::FILTER_CREATED_ALL => Yii::t('app', 'All cases '),
         ];
     }
 
-    public function getCreatedAtFilters()
+    public function getStatusFilters($action)
     {
-        if (!$this->filter_created_at_list) {
-            $this->initCreatedAtFilters();
+        switch ($action) {
+            case 'search':
+                return [
+                    [
+                        'label' => Yii::t('app', 'Show only incomplete records'),
+                        'value' => self::FILTER_STATUS_INCOMPLETE,
+                    ],
+                    [
+                        'label' => Yii::t('app', 'Show only records within deactivation window'),
+                        'value' => self::FILTER_STATUS_COMPLETE,
+                    ],
+                ];
         }
-
-        return $this->filter_created_at_list;
     }
 
-    private function initUploaderList()
+    public function getAuthorFilters()
     {
-        $this->uploader_list[0] = Yii::t('app', 'all');
+        $authors = [Yii::t('app', 'all')];
         foreach ($this->getHistory() as $history) {
             $author = $history->author;
             $full_name = trim($author->getFullName());
-            $this->uploader_list[$author->id] = !$full_name ? '# ' . $author->id : $full_name . ' / ' . $author->id;
+            $authors[$author->id] = !$full_name ? '# ' . $author->id : $full_name . ' / ' . $author->id;
         }
 
-        return $this->uploader_list;
+        return $authors;
     }
 
     /**
@@ -207,12 +161,4 @@ class Record extends \app\modules\frontend\models\base\Record
             ->all();
     }
 
-    public function getUploaderList()
-    {
-        if (!$this->uploader_list) {
-            $this->initUploaderList();
-        }
-
-        return $this->uploader_list;
-    }
 }
