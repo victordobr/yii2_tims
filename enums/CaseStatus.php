3be9a0ef756a1142c8639d0e434810a7f0cb4158
Alex Makhorin <despected@gmail.com>
2016-02-12 11:56:23 +0200

    listCodeDescription()

--- enums/CaseStatus.php
+++ enums/CaseStatus.php
@@ -55,68 +55,78 @@ class CaseStatus extends Enum
     const OVERDUE_P2 = 5061;
     const SENT_TO_COLLECTIONS = 5070;
     const HOLD_5999 = 5999;
-    
+
     const CLOSED_PAID = 6010;
     const CLOSED_UNPAID = 6011;
     const ARCHIVED = 6020;
     const RECORD_HOLD = 6021;
     const MARKED_FOR_PURGE_30D = 6022;
 
+    public static function listCodeDescription()
+    {
+        $list = [];
+        foreach (self::listMainText() as $key => $value) {
+            $list[$key] = $key . ' - ' . $value;
+        }
+
+        return $list;
+    }
+
     public static function listMainText()
     {
         return [
-            self::INCOMPLETE  => Yii::t('app', 'Incomplete – Pending Upload Finalisation'), //1010
-            self::COMPLETE  => Yii::t('app', 'Waiting for Evidence Review'), //1020
-            self::FULL_COMPLETE  => Yii::t('app', 'Waiting for Evidence Review'), //1021
-            self::VIEWED_RECORD  => Yii::t('app', 'Waiting for Evidence Review'), //2010
-            self::AWAITING_DEACTIVATION  => Yii::t('app', 'Waiting for Deactivation'), //1030
-            self::DEACTIVATED_RECORD  => Yii::t('app', 'Deactivated Case'), //1040
-            self::APPROVED_RECORD  => Yii::t('app', 'Waiting for DMV Data'), //2020
-            self::APPROVED_RECORD_2021  => Yii::t('app', 'Waiting for DMV Data'), //2021
-            self::QUERY_SUBMITTED  => Yii::t('app', 'Waiting for DMV Data'), //3010
-            self::REJECTED_RECORD  => Yii::t('app', 'Rejected Case'), //2030
-            self::REJECTED_RECORD_2031  => Yii::t('app', 'Rejected Case'), //2031
-            self::DMV_DATA_RETRIEVED_COMPLETE  => Yii::t('app', 'Waiting for Citation Print/Mail'), //3020
-            self::DMV_DATA_RETRIEVED_INCOMPLETE  => Yii::t('app', 'Waiting for Citation Print/Mail'), //3021
-            self::DMV_DATA_RETRIEVED_INCOMPLETE_CRITICAL  => Yii::t('app', 'NLETS ERR'), //3030
-            self::DMV_DATA_CORRUPT  => Yii::t('app', 'NLETS ERR'), //3031
-            self::DMV_DATA_NOT_AVAILABLE  => Yii::t('app', 'NLETS ERR'), //3032
-            self::DMV_DATA_MULTIPLE_MATCH  => Yii::t('app', 'NLETS ERR'), //3033
-            self::PRINTED_P1  => Yii::t('app', 'Citation Printed/Mailed (Period X)'), //4010
-            self::PRINTED_P2  => Yii::t('app', 'Citation Printed/Mailed (Period X)'), //4040
-            self::VIEWED_RECORD_P1  => Yii::t('app', 'Violator Received, Waiting for Payment'), //5011
-            self::VIEWED_RECORD_P2  => Yii::t('app', 'Violator Received, Waiting for Payment'), //5021
-            self::PRINTED_P1  => Yii::t('app', 'Payment Overdue'), //4010/5011
-            self::PAID  => Yii::t('app', 'Paid'), //5030
+            self::INCOMPLETE => Yii::t('app', 'Incomplete – Pending Upload Finalisation'), //1010
+            self::COMPLETE => Yii::t('app', 'Waiting for Evidence Review'), //1020
+            self::FULL_COMPLETE => Yii::t('app', 'Waiting for Evidence Review'), //1021
+            self::VIEWED_RECORD => Yii::t('app', 'Waiting for Evidence Review'), //2010
+            self::AWAITING_DEACTIVATION => Yii::t('app', 'Waiting for Deactivation'), //1030
+            self::DEACTIVATED_RECORD => Yii::t('app', 'Deactivated Case'), //1040
+            self::APPROVED_RECORD => Yii::t('app', 'Waiting for DMV Data'), //2020
+            self::APPROVED_RECORD_2021 => Yii::t('app', 'Waiting for DMV Data'), //2021
+            self::QUERY_SUBMITTED => Yii::t('app', 'Waiting for DMV Data'), //3010
+            self::REJECTED_RECORD => Yii::t('app', 'Rejected Case'), //2030
+            self::REJECTED_RECORD_2031 => Yii::t('app', 'Rejected Case'), //2031
+            self::DMV_DATA_RETRIEVED_COMPLETE => Yii::t('app', 'Waiting for Citation Print/Mail'), //3020
+            self::DMV_DATA_RETRIEVED_INCOMPLETE => Yii::t('app', 'Waiting for Citation Print/Mail'), //3021
+            self::DMV_DATA_RETRIEVED_INCOMPLETE_CRITICAL => Yii::t('app', 'NLETS ERR'), //3030
+            self::DMV_DATA_CORRUPT => Yii::t('app', 'NLETS ERR'), //3031
+            self::DMV_DATA_NOT_AVAILABLE => Yii::t('app', 'NLETS ERR'), //3032
+            self::DMV_DATA_MULTIPLE_MATCH => Yii::t('app', 'NLETS ERR'), //3033
+            self::PRINTED_P1 => Yii::t('app', 'Citation Printed/Mailed (Period X)'), //4010
+            self::PRINTED_P2 => Yii::t('app', 'Citation Printed/Mailed (Period X)'), //4040
+            self::VIEWED_RECORD_P1 => Yii::t('app', 'Violator Received, Waiting for Payment'), //5011
+            self::VIEWED_RECORD_P2 => Yii::t('app', 'Violator Received, Waiting for Payment'), //5021
+            self::PRINTED_P1 => Yii::t('app', 'Payment Overdue'), //4010/5011
+            self::PAID => Yii::t('app', 'Paid'), //5030
         ];
     }
 
     public static function listSubText($dictionary = [])
     {
         return [
-            self::INCOMPLETE  => Yii::t('app', '(upload deadline in {N} days)', $dictionary), //1010
-            self::COMPLETE  => Yii::t('app', '(review deadline in {N} days)', $dictionary), //1020
-            self::FULL_COMPLETE  => Yii::t('app', '(review deadline in {N} days)', $dictionary), //1021
-            self::VIEWED_RECORD  => Yii::t('app', '(review deadline in {N} days)', $dictionary), //2010
-            self::AWAITING_DEACTIVATION  => Yii::t('app', '(requested by {requester_username}, {datetime})', $dictionary), //1030
-            self::DEACTIVATED_RECORD  => Yii::t('app', '(deactivated by {requester_username}, {datetime})', $dictionary), //1040
-            self::APPROVED_RECORD  => Yii::t('app', '(print deadline in {N} days)', $dictionary), //2020
-            self::APPROVED_RECORD_2021  => Yii::t('app', '(print deadline in {N} days)', $dictionary), //2021
-            self::QUERY_SUBMITTED  => Yii::t('app', '(print deadline in {N} days)', $dictionary), //3010
-            self::REJECTED_RECORD  => Yii::t('app', '(rejected by {approvers_username}, {datetime})', $dictionary), //2030
-            self::REJECTED_RECORD_2031  => Yii::t('app', '(rejected by {approvers_username}, {datetime})', $dictionary), //2031
-            self::DMV_DATA_RETRIEVED_COMPLETE  => Yii::t('app', '(print deadline in {N} days)', $dictionary), //3020
-            self::DMV_DATA_RETRIEVED_INCOMPLETE  => Yii::t('app', '(print deadline in {N} days)', $dictionary), //3021
-            self::DMV_DATA_RETRIEVED_INCOMPLETE_CRITICAL  => Yii::t('app', '(print deadline in {N} days)', $dictionary), //3030
-            self::DMV_DATA_CORRUPT  => Yii::t('app', '(print deadline in {N} days)', $dictionary), //3031
-            self::DMV_DATA_NOT_AVAILABLE  => Yii::t('app', '(print deadline in {N} days)', $dictionary), //3032
-            self::DMV_DATA_MULTIPLE_MATCH  => Yii::t('app', '(print deadline in {N} days)', $dictionary), //3033
-            self::PRINTED_P1  => Yii::t('app', '(printed by {operator_username}, {datetime})', $dictionary), //4010
-            self::PRINTED_P2  => Yii::t('app', '(printed by {operator_username}, {datetime})', $dictionary), //4040
-            self::VIEWED_RECORD_P1  => Yii::t('app', '(due in {N} days)', $dictionary), //5011
-            self::VIEWED_RECORD_P2  => Yii::t('app', '(due in {N} days)', $dictionary), //5021
-            self::PRINTED_P1  => Yii::t('app', '(print P2 citation for mailing)', $dictionary), //4010/5011
-            self::PAID  => Yii::t('app', '(payment received {datetime})', $dictionary), //5030
+            self::INCOMPLETE => Yii::t('app', '(upload deadline in {N} days)', $dictionary), //1010
+            self::COMPLETE => Yii::t('app', '(review deadline in {N} days)', $dictionary), //1020
+            self::FULL_COMPLETE => Yii::t('app', '(review deadline in {N} days)', $dictionary), //1021
+            self::VIEWED_RECORD => Yii::t('app', '(review deadline in {N} days)', $dictionary), //2010
+            self::AWAITING_DEACTIVATION => Yii::t('app', '(requested by {requester_username}, {datetime})', $dictionary), //1030
+            self::DEACTIVATED_RECORD => Yii::t('app', '(deactivated by {requester_username}, {datetime})', $dictionary), //1040
+            self::APPROVED_RECORD => Yii::t('app', '(print deadline in {N} days)', $dictionary), //2020
+            self::APPROVED_RECORD_2021 => Yii::t('app', '(print deadline in {N} days)', $dictionary), //2021
+            self::QUERY_SUBMITTED => Yii::t('app', '(print deadline in {N} days)', $dictionary), //3010
+            self::REJECTED_RECORD => Yii::t('app', '(rejected by {approvers_username}, {datetime})', $dictionary), //2030
+            self::REJECTED_RECORD_2031 => Yii::t('app', '(rejected by {approvers_username}, {datetime})', $dictionary), //2031
+            self::DMV_DATA_RETRIEVED_COMPLETE => Yii::t('app', '(print deadline in {N} days)', $dictionary), //3020
+            self::DMV_DATA_RETRIEVED_INCOMPLETE => Yii::t('app', '(print deadline in {N} days)', $dictionary), //3021
+            self::DMV_DATA_RETRIEVED_INCOMPLETE_CRITICAL => Yii::t('app', '(print deadline in {N} days)', $dictionary), //3030
+            self::DMV_DATA_CORRUPT => Yii::t('app', '(print deadline in {N} days)', $dictionary), //3031
+            self::DMV_DATA_NOT_AVAILABLE => Yii::t('app', '(print deadline in {N} days)', $dictionary), //3032
+            self::DMV_DATA_MULTIPLE_MATCH => Yii::t('app', '(print deadline in {N} days)', $dictionary), //3033
+            self::PRINTED_P1 => Yii::t('app', '(printed by {operator_username}, {datetime})', $dictionary), //4010
+            self::PRINTED_P2 => Yii::t('app', '(printed by {operator_username}, {datetime})', $dictionary), //4040
+            self::VIEWED_RECORD_P1 => Yii::t('app', '(due in {N} days)', $dictionary), //5011
+            self::VIEWED_RECORD_P2 => Yii::t('app', '(due in {N} days)', $dictionary), //5021
+            self::PRINTED_P1 => Yii::t('app', '(print P2 citation for mailing)', $dictionary), //4010/5011
+            self::PAID => Yii::t('app', '(payment received {datetime})', $dictionary), //5030
         ];
     }
 }
