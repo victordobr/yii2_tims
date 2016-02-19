7a680de0ea8ee5ad752ad3c25b31f07c26d67ee9
eugene <egichko@entenso.com>
2016-02-19 17:07:15 +0200

    added init, check and get current tab

--- modules/frontend/Module.php
+++ modules/frontend/Module.php
@@ -6,12 +6,18 @@ use \Yii;
 use \yii\filters\AccessControl;
 use \app\interfaces\Menu as MenuInterface;
 use yii\bootstrap\Html;
+use yii\helpers\BaseUrl;
 
 /**
  * Web module for administration panel.
  */
 class Module extends \app\base\Module  implements MenuInterface
 {
+    /**
+     * @var string current tab
+     */
+    private static $tab;
+
     /** @var string $controllerNamespace controller namespace */
     public $controllerNamespace = 'app\modules\frontend\controllers';
 
@@ -50,16 +56,64 @@ class Module extends \app\base\Module  implements MenuInterface
         ];
     }
 
+    /**
+     * @return string
+     */
+    public static function getTab()
+    {
+        if (!self::$tab) {
+            self::initCurrentTab();
+        }
+
+        return self::$tab;
+    }
+
+    /**
+     * @param string $tab
+     * @return bool
+     */
+    public static function isCurrentTab($tab)
+    {
+        if (!self::$tab) {
+            self::initCurrentTab();
+        }
+
+        return self::$tab == $tab;
+    }
+
+    /**
+     * @return string
+     */
+    private static function initCurrentTab()
+    {
+        $url = Yii::$app->request->url;
+
+        switch (true) {
+            case !is_bool(strpos($url, 'search')):
+                return self::$tab = 'search';
+            case !is_bool(strpos($url, 'review')):
+                return self::$tab =  'review';
+            case !is_bool(strpos($url, 'print')):
+                return self::$tab =  'print';
+            case !is_bool(strpos($url, 'update')):
+                return self::$tab =  'update';
+            case !is_bool(strpos($url, 'reports')):
+                return self::$tab =  'reports';
+            case !is_bool(strpos($url, 'settings')):
+                return self::$tab =  'settings';
+            default:
+                return self::$tab =  'undefined';
+        }
+    }
+
     public static function getMenuItems()
     {
-        $action = Yii::$app->controller->action->id;
-
         return [
             ['encode' => false, 'label' => Html::icon('upload') . '&nbsp;&nbsp;' . Yii::t('app', 'Upload'), 'url' => ['/frontend/records/upload']],
-            ['encode' => false, 'label' => Html::icon('search') . '&nbsp;&nbsp;' . Yii::t('app', 'Search'), 'url' => ['/frontend/records/search'], 'active' => in_array($action, ['search', 'SearchDetail'])],
-            ['encode' => false, 'label' => Html::icon('eye-open') . '&nbsp;&nbsp;' . Yii::t('app', 'Review'), 'url' => ['/frontend/records/review'], 'active' => in_array($action, ['review', 'ReviewDetail'])],
-            ['encode' => false, 'label' => Html::icon('print') . '&nbsp;&nbsp;' . Yii::t('app', 'Print'), 'url' => ['/frontend/print/index'], 'active' => Yii::$app->controller->id == 'print'],
-            ['encode' => false, 'label' => Html::icon('pencil') . '&nbsp;&nbsp;' . Yii::t('app', 'Update'), 'url' => false, 'options' => ['class' => 'disabled']],
+            ['encode' => false, 'label' => Html::icon('search') . '&nbsp;&nbsp;' . Yii::t('app', 'Search'), 'url' => ['/search'], 'active' => self::isCurrentTab('search')],
+            ['encode' => false, 'label' => Html::icon('eye-open') . '&nbsp;&nbsp;' . Yii::t('app', 'Review'), 'url' => ['/review'], 'active' => self::isCurrentTab('review')],
+            ['encode' => false, 'label' => Html::icon('print') . '&nbsp;&nbsp;' . Yii::t('app', 'Print'), 'url' => ['/print'], 'active' =>  self::isCurrentTab('print')],
+            ['encode' => false, 'label' => Html::icon('pencil') . '&nbsp;&nbsp;' . Yii::t('app', 'Update'), 'url' => ['/update'], 'active' =>  self::isCurrentTab('update')],
             ['encode' => false, 'label' => Html::icon('list-alt') . '&nbsp;&nbsp;' . Yii::t('app', 'Reports'), 'url' => false, 'options' => ['class' => 'disabled']],
             ['encode' => false, 'label' => Html::icon('asterisk') . '&nbsp;&nbsp;' . Yii::t('app', 'Settings'), 'url' => false, 'options' => ['class' => 'disabled']],
         ];
