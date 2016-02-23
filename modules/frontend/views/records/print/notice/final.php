05527fbfefd53e8a66ec8de56ae98cc9500cb92c
eugene <egichko@entenso.com>
2016-02-23 18:54:10 +0200

    parsed notices from pdf to html

---
+++ modules/frontend/views/records/print/notice/final.php
@@ -0,0 +1,118 @@
+<div id="jpedal" style="overflow: hidden; position: relative; width: 825px; height: 1100px;">
+    <div id="t1_2" class="t s1_2">J O N E S C O U N T Y , G E O R G I A</div>
+    <div id="t2_2" class="t s2_2">TRAFFIC CITATION FOR STOPARM VIOLATION</div>
+    <div id="t3_2" class="t s3_2">NCIC Number</div>
+    <div id="t4_2" class="t s4_2">SS NR-GA0840000</div>
+    <div id="t5_2" class="t s3_2">Citation Number</div>
+    <div id="t6_2" class="t s4_2">JCA-000001</div>
+    <div id="t7_2" class="t s4_2">THEODORE MONTGOMERY</div>
+    <div id="t8_2" class="t s5_2">3240 164TH ST SUITE 520</div>
+    <div id="t9_2" class="t s5_2">HAMPSHIRE PLACE GA 31032</div>
+    <div id="ta_2" class="t m1_2 s6_2"></div>
+    <div id="tb_2" class="t s7_2">Citation Number</div>
+    <div id="tc_2" class="t s7_2">:</div>
+    <div id="td_2" class="t s8_2">JCA-000001</div>
+    <div id="te_2" class="t s9_2">FINAL NOTICE</div>
+    <div id="tf_2" class="t s10_2">VIEW THIS VIOLATION AND PAY ONLINE AT </div>
+    <div id="tg_2" class="t s11_2">WWW.STOP4THEBUS.COM</div>
+    <div id="th_2" class="t s10_2">OR CALL 1-844-901-STOP</div>
+    <div id="ti_2" class="t m2_2 s4_2">REGISTERED OWNER</div>
+    <div id="tj_2" class="t m2_2 s4_2">VIOLATION DETAILS</div>
+    <div id="tk_2" class="t s3_2">This citation shall constitute official notice to you that the vehicle shown in this photographic record </div>
+    <div id="tl_2" class="t s3_2">was operated in disregard or disobedience of subsection (a) of this Code section O.C.G.A. § 40-6-163 </div>
+    <div id="tm_2" class="t s3_2">(see overleaf), proof of which is evidenced by these recorded images.</div>
+    <div id="tn_2" class="t s3_2">These recorded images have been reviewed and approved by a certified peace officer employed by a </div>
+    <div id="to_2" class="t s3_2">law enforcement agency authorized to enforce this Code section O.C.G.A. § 40-6-163, and based upon </div>
+    <div id="tp_2" class="t s3_2">inspection of recorded images, the owner’s motor vehicle was found to have been operated in </div>
+    <div id="tq_2" class="t s3_2">disregard or disobedience of subsection (a) of this Code section O.C.G.A. § 40-6-163 and that such </div>
+    <div id="tr_2" class="t s3_2">disregard or disobedience was not otherwise authorized by law.</div>
+    <div id="ts_2" class="t s3_2">Please note that the registrant of the motor vehicle is legally responsible for this violation (see overleaf </div>
+    <div id="tt_2" class="t s3_2">- subsection (d)(3)(D) of this Code section O.C.G.A. § 40-6-163). If you are not the driver of the vehicle </div>
+    <div id="tu_2" class="t s3_2">at the time of the alleged violation, you may request a court date to contest this alleged violation (see </div>
+    <div id="tv_2" class="t s3_2">overleaf - subsection (d)(3)(D) parts (i) and (ii) of this Code section O.C.G.A. § 40-6-163).</div>
+    <div id="tw_2" class="t s3_2">If you do not make payment, or request a court date, by the deadline noted on this citation, as per </div>
+    <div id="tx_2" class="t s3_2">subsection (d)(5) of this Code section O.C.G.A. § 40-6-163 (see overleaf), you shall be deemed to have </div>
+    <div id="ty_2" class="t s3_2">waived any and all rights to contest the alleged violation and shall become immediately liable for the </div>
+    <div id="tz_2" class="t s3_2">civil monetary penalty as stated in this citation.</div>
+    <div id="t10_2" class="t m2_2 s4_2">SUMMONS</div>
+    <div id="t11_2" class="t s12_2">Reviewed and Confirmed by:</div>
+    <div id="t12_2" class="t s12_2">Officer</div>
+    <div id="t13_2" class="t s12_2">: </div>
+    <div id="t14_2" class="t s12_2">Holly Hunter</div>
+    <div id="t15_2" class="t s12_2">Badge Number</div>
+    <div id="t16_2" class="t s12_2">: </div>
+    <div id="t17_2" class="t s12_2">JCPD 4080</div>
+    <div id="t18_2" class="t s12_2">Jones County, Georgia</div>
+    <div id="t19_2" class="t s7_2">Notice Date</div>
+    <div id="t1a_2" class="t s7_2">:</div>
+    <div id="t1b_2" class="t s8_2">01 NOV 2015</div>
+    <div id="t1c_2" class="t s7_2">Plate/State</div>
+    <div id="t1d_2" class="t s7_2">:</div>
+    <div id="t1e_2" class="t s8_2">DR4 60N / GA</div>
+    <div id="t1f_2" class="t s7_2">Make/Model/Year :</div>
+    <div id="t1g_2" class="t s8_2">HONDA/CIVIC/2010</div>
+    <div id="t1h_2" class="t s7_2">Viol. Date/Time</div>
+    <div id="t1i_2" class="t s7_2">:</div>
+    <div id="t1j_2" class="t s8_2">30 OCT 2015 / 8:12 AM</div>
+    <div id="t1k_2" class="t s7_2">Viol. Location</div>
+    <div id="t1l_2" class="t s7_2">:</div>
+    <div id="t1m_2" class="t s8_2">1628 N OAKSHOT ST</div>
+    <div id="t1n_2" class="t m2_2 s4_2">RESPONSE STUB</div>
+    <div id="t1o_2" class="t s13_2">E N C L O S E S T U B W H E N R E T U R N I N G </div>
+    <div id="t1p_2" class="t s13_2">• </div>
+    <div id="t1q_2" class="t s13_2">A L L O W 5 D A Y S F O R D E L I V E R Y </div>
+    <div id="t1r_2" class="t s14_2">• </div>
+    <div id="t1s_2" class="t s13_2">D O N O T S E N D C A S H </div>
+    <div id="t1t_2" class="t s14_2">• </div>
+    <div id="t1u_2" class="t s13_2">D O N O T S T A P L E C H E C K T O T H I S F O R M</div>
+    <div id="t1v_2" class="t s7_2">Viol. Nature</div>
+    <div id="t1w_2" class="t s7_2">:</div>
+    <div id="t1x_2" class="t s8_2">VEHICLE WAS OPERATED IN </div>
+    <div id="t1y_2" class="t s8_2">DISREGARD OR DISOBEDIENCE</div>
+    <div id="t1z_2" class="t s8_2">OF O.C.G.A. </div>
+    <div id="t20_2" class="t s15_2">§</div>
+    <div id="t21_2" class="t s8_2">40-6-163 (2014)</div>
+    <div id="t22_2" class="t s8_2">SUBSECTION (A)</div>
+    <div id="t23_2" class="t s16_2">TOTAL AMOUNT DUE:</div>
+    <div id="t24_2" class="t s17_2">$315</div>
+    <div id="t25_2" class="t s18_2">YOU MUST REMIT PAYMENT OF THE </div>
+    <div id="t26_2" class="t s18_2">APPLICABLE FINE OR REQUEST A </div>
+    <div id="t27_2" class="t s18_2">COURT DATE TO CONTEST THIS </div>
+    <div id="t28_2" class="t s18_2">ALLEGED VIOLATION BY THE DATE </div>
+    <div id="t29_2" class="t s18_2">SHOWN BELOW.</div>
+    <div id="t2a_2" class="t s16_2">PAY BY / CONTEST BY:</div>
+    <div id="t2b_2" class="t s19_2">14 JAN 2016</div>
+    <div id="t2c_2" class="t s4_2">MAKE ALL CHEQUES PAYABLE TO:</div>
+    <div id="t2d_2" class="t s12_2">TOTAL DUE: </div>
+    <div id="t2e_2" class="t s9_2">$315</div>
+    <div id="t2f_2" class="t s7_2">Unique Passcode:</div>
+    <div id="t2g_2" class="t s8_2">4KF8AC6DV8FG</div>
+    <div id="t2h_2" class="t s7_2">Pay By / Contest By:</div>
+    <div id="t2i_2" class="t s8_2">14 JAN 2016</div>
+    <div id="t2j_2" class="t s3_2">If you choose to mail in your response, please select an option from </div>
+    <div id="t2k_2" class="t s3_2">the below, and mail this stub back to the listed address:</div>
+    <div id="t2l_2" class="t s3_2">Payment enclosed</div>
+    <div id="t2m_2" class="t s3_2">Request court date</div>
+    <div id="t2n_2" class="t s7_2">Citation Number:</div>
+    <div id="t2o_2" class="t s8_2">JCA-000001</div>
+    <div id="t2p_2" class="t s3_2">View the violation, pay your ticket, or request a court date </div>
+    <div id="t2q_2" class="t s3_2">online at </div>
+    <div id="t2r_2" class="t s3_2">www.stop4thebus.com.</div>
+    <div id="t2s_2" class="t s7_2">Location Code:</div>
+    <div id="t2t_2" class="t s8_2">JCA</div>
+    <div id="t2u_2" class="t s3_2">Violation Penalty: </div>
+    <div id="t2v_2" class="t s3_2">300</div>
+    <div id="t2w_2" class="t s3_2">Processing Fee (5%): </div>
+    <div id="t2x_2" class="t s3_2">15</div>
+    <div id="t2y_2" class="t s4_2">RS TICKET PROCESSING SERVICES</div>
+    <div id="t2z_2" class="t s5_2">446 HARRISON STREET</div>
+    <div id="t30_2" class="t s5_2">SUMAS WA 98295</div>
+
+    <!-- End text definitions -->
+
+    <!-- Begin page background -->
+    <div id="pg2Overlay" style="width:100%; height:100%; position:absolute; z-index:1; background-color:rgba(0,0,0,0); -webkit-user-select: none;"></div>
+    <div id="pg2"><object width="825" height="1100" data="<?=$svg?>" type="image/svg+xml" id="pdf2" style="width:825px; height:1100px; background-color:white; -moz-transform:scale(1); z-index: 0;"></object></div>
+    <!-- End page background -->
+
+</div>
