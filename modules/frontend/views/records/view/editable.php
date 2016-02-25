6c1782f5e6786c8636125ce76af9bc822fec6b10
eugene <egichko@entenso.com>
2016-02-25 12:47:05 +0200

    corrected record editable view

--- modules/frontend/views/records/view/editable.php
+++ modules/frontend/views/records/view/editable.php
@@ -2,12 +2,13 @@
 
 use app\widgets\base\DetailView;
 use kartik\icons\Icon;
-use yii\helpers\Html;
+use yii\widgets\Pjax;
+use yii\widgets\ActiveForm;
+use yii\widgets\MaskedInput;
 
 /**
  * @var $this yii\web\View
  * @var $model \app\models\Record
- * @var $form string
  * @var $formatter \app\helpers\Formatter
  * @var $statuses array
  */
@@ -15,12 +16,28 @@ use yii\helpers\Html;
 $formatter = Yii::$app->formatter;
 ?>
 
-<div class="record-view-full-details">
+<div id="record-editable-view">
 
     <div class="col-xs-12">
 
         <div class="row">
             <div class="col-xs-6">
+                <?php Pjax::begin([
+                    'id' => 'pjax-case-details',
+                    'timeout' => false,
+                    'enablePushState' => false,
+                    'formSelector' => '#form-case-details',
+                    'options' => ['class' => 'wrapper-detail-view',]
+                ]); ?>
+                <?php $form = ActiveForm::begin([
+                    'id' => 'form-case-details',
+                    'method' => 'GET',
+                    'enableClientScript' => false,
+                    'options' => ['data-pjax' => true],
+                    'fieldConfig' => [
+                        'template' => '<div class="col-lg-12">{label}{input}{error}</div>',
+                    ],
+                ]); ?>
                 <?= DetailView::widget([
                     'id' => 'case-details',
                     'editable' => true,
@@ -50,14 +67,34 @@ $formatter = Yii::$app->formatter;
                             'attribute' => 'statusName',
                         ],
                         [
+                            'format' => 'raw',
                             'label' => Yii::t('app', 'Change record state to'),
-                            'value' => $this->render('partials/dropdown-statuses', ['statuses' => $statuses]),
-                            'format' => 'raw'
+                            'value' => $form->field($model, 'status_id', [
+                                'options' => ['class' => 'form-group form-group-sm'],
+                            ])->dropDownList($statuses)->label(false),
                         ]
                     ],
                 ]) ?>
+                <?php ActiveForm::end(); ?>
+                <?php Pjax::end(); ?>
             </div>
             <div class="col-xs-6">
+                <?php Pjax::begin([
+                    'id' => 'pjax-photo-video-evidence',
+                    'timeout' => false,
+                    'enablePushState' => false,
+                    'formSelector' => '#form-photo-video-evidence',
+                    'options' => ['class' => 'wrapper-detail-view',]
+                ]); ?>
+                <?php $form = ActiveForm::begin([
+                    'id' => 'form-photo-video-evidence',
+                    'enableClientScript' => false,
+                    'method' => 'GET',
+                    'options' => ['data-pjax' => true],
+                    'fieldConfig' => [
+                        'template' => '<div class="col-lg-12">{label}{input}{error}</div>',
+                    ],
+                ]); ?>
                 <?= DetailView::widget([
                     'id' => 'photo-video-evidence',
                     'editable' => true,
@@ -69,12 +106,20 @@ $formatter = Yii::$app->formatter;
                         [
                             'format' => 'raw',
                             'label' => Yii::t('app', 'GPS Latitude'),
-                            'value' => $this->render('partials/input-latitude', ['location' => $model->location]),
+                            'value'=> $form->field($model->location, 'lat_ddm', [
+                                'options' => ['class' => 'form-group form-group-sm'],
+                            ])->widget(MaskedInput::classname(), [
+                            'mask' =>  '9[9[9]].9[9[9]].9[9[9[9[9]]]]a',
+                        ])->label(false)->hint('(eg. 122.21.73060W)'),
                         ],
                         [
                             'format' => 'raw',
                             'label' => Yii::t('app', 'GPS Longitude'),
-                            'value' => $this->render('partials/input-longitude', ['location' => $model->location]),
+                            'value'=> $form->field($model->location, 'lng_ddm', [
+                                'options' => ['class' => 'form-group form-group-sm'],
+                            ])->widget(MaskedInput::classname(), [
+                                'mask' =>  '9[9[9]].9[9[9]].9[9[9[9[9]]]]a',
+                            ])->label(false)->hint('(eg. 122.21.73060W)'),
                         ],
                         [
                             'label' => Yii::t('app', 'Location (nearby address)'),
@@ -83,6 +128,8 @@ $formatter = Yii::$app->formatter;
                     ],
                     'footer' => $this->render('../../partials/evidence', ['model' => $model])
                 ]) ?>
+                <?php ActiveForm::end(); ?>
+                <?php Pjax::end(); ?>
             </div>
         </div>
 
