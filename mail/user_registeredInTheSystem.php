b73e096502843970f72399f36b35fa140d6fbb27
Alex Makhorin <despected@gmail.com>
2016-03-05 18:40:57 +0200

    Fixed actvation email sending

--- mail/user_registeredInTheSystem.php
+++ mail/user_registeredInTheSystem.php
@@ -5,7 +5,9 @@
 
 <p>You have been successfully registered.</p>
 
-<p>Please use these credentials to log in:</p>
+<p>You need to activate your account. Go to link <a href="<?= $activationUrl;?>"><?= $activationUrl;?></a></p>
+
+<p>After activation please use these credentials to log in:</p>
 
 <p><strong>Login:</strong> <?= $login;?></p>
 
