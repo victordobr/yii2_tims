64521a9bee640787fbd3e55b31cc1765909167a9
Alex Makhorin <despected@gmail.com>
2016-03-10 11:52:39 +0200

    migrations fix

--- migrations/m160301_153353_Settings_table_create.php
+++ migrations/m160301_153353_Settings_table_create.php
@@ -6,23 +6,23 @@ class m160301_153353_Settings_table_create extends Migration
 {
     public function up()
     {
-        $this->createTable(
-            '{{%settings}}',
-            [
-                'id' => $this->primaryKey(),
-                'type' => $this->string(255)->notNull(),
-                'section' => $this->string(255)->notNull(),
-                'key' => $this->string(255)->notNull(),
-                'value' => $this->text(),
-                'active' => $this->boolean(),
-                'created' => $this->dateTime(),
-                'modified' => $this->dateTime(),
-            ]
-        );
+//        $this->createTable(
+//            '{{%settings}}',
+//            [
+//                'id' => $this->primaryKey(),
+//                'type' => $this->string(255)->notNull(),
+//                'section' => $this->string(255)->notNull(),
+//                'key' => $this->string(255)->notNull(),
+//                'value' => $this->text(),
+//                'active' => $this->boolean(),
+//                'created' => $this->dateTime(),
+//                'modified' => $this->dateTime(),
+//            ]
+//        );
     }
 
     public function down()
     {
-        $this->dropTable('{{%settings}}');
+//        $this->dropTable('{{%settings}}');
     }
 }
--- migrations/m160301_153651_Settings_table_add_unique_index.php
+++ migrations/m160301_153651_Settings_table_add_unique_index.php
@@ -6,11 +6,11 @@ class m160301_153651_Settings_table_add_unique_index extends Migration
 {
     public function safeUp()
     {
-        $this->createIndex('settings_unique_key_section', '{{%settings}}', ['section', 'key'], true);
+//        $this->createIndex('settings_unique_key_section', '{{%settings}}', ['section', 'key'], true);
     }
 
     public function safeDown()
     {
-        $this->dropIndex('settings_unique_key_section', '{{%settings}}');
+//        $this->dropIndex('settings_unique_key_section', '{{%settings}}');
     }
 }
