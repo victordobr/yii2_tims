4c55b2fa01e22b1651902ba92548bceaf0fc993a
eugene <egichko@entenso.com>
2016-03-16 18:40:17 +0200

    added location codes enum

---
+++ enums/location/Code.php
@@ -0,0 +1,14 @@
+<?php
+namespace app\enums\location;
+
+use Yii;
+
+/**
+ * Location codes Enum
+ * @package app\enums
+ * @author Eugene Gichko <eugene.gichko@gmail.com>
+ */
+class Code
+{
+    const JCA = 'JCA';
+}
