7dda618b6b62c0c9f52ba4d1d2dd0291bfcf904e
Eugene <g14ko@yandex.ru>
2016-03-24 00:27:37 +0200

    customize front end

--- modules/frontend/views/layouts/one-column.php
+++ modules/frontend/views/layouts/one-column.php
@@ -3,6 +3,9 @@
  * @var \yii\web\View $this
  * @var string $content
  */
+
+use yii\helpers\Html;
+use yii\helpers\Url;
 ?>
 
 <?php $this->beginPage() ?>
@@ -19,6 +22,28 @@
 <?php $this->beginBody() ?>
 <div class="wrap frontend-wrapper">
 
+    <header>
+        <div class="container">
+            <?= \app\widgets\user\info\Info::widget(); ?>
+            <?= Html::tag('h1', Yii::t('app', '{logo} Traffic Infraction Management System', ['logo' => Html::tag('span', 'TIMS', ['class' => 'title-abbr'])])); ?>
+        </div>
+        <div class="sub-title">
+            <div class="container">
+
+                <div class="control-group pull-right">
+                    <?= Html::a('User profile', Url::to('/frontend/default/profile'))?>
+                    <?php if(Yii::$app->user->hasRole([\app\enums\Role::ROLE_ROOT_SUPERUSER])):?>
+                        <?= Html::a('Admin portal', Url::to('/admin/users/manage'))?>
+                    <?php endif;?>
+                    <?= Html::a('Logout', Url::to('/logout'))?>
+                </div>
+
+                <?= Html::tag('h2', Yii::t('app', 'Jones County, Georgia')); ?>
+
+            </div>
+        </div>
+    </header>
+
     <div class="container">
         <div id="page-loading">
             <div class="show-loading"></div>
@@ -27,7 +52,6 @@
 
         <div class="row">
             <div class="col-md-9">
-                <div class="header">BANNER/HEADER</div>
                 <div class="top-menu">
                     <?= yii\bootstrap\Nav::widget([
                         'options' => ['class' => 'nav nav-tabs nav-justified'],
@@ -50,7 +74,6 @@
 
 <footer class="footer">
     <div class="container">
-        <p class="pull-right"><img src="/images/evidence-investigation.png" alt="evidence & investigation" height="47"></p>
     </div>
 </footer>
 
